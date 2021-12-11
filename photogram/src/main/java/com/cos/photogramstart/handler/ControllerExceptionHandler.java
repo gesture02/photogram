678b704f2972dff1f6e3a326d903e9e16eda340a
gesture02 <gesture02@gmail.com>
2021-12-11 20:23:33 +0900

    script resp

--- photogram/src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ photogram/src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.util.Script;
 import com.cos.photogramstart.web.dto.CMRespDto;
 
 @RestController
@@ -14,7 +15,10 @@ import com.cos.photogramstart.web.dto.CMRespDto;
 public class ControllerExceptionHandler {
 	
 	@ExceptionHandler(CustomValidationException.class)
-	public CMRespDto<?> validationException(CustomValidationException e) {
-		return new CMRespDto<Map<String, String>>(-1, e.getMessage(), e.getErrorMap());
+	public String validationException(CustomValidationException e) {
+		//1. 클라이언트에게 응답 : Script가 좋음
+		//2. Ajax통신(개발자가 응답받음) - CMRespDto
+		//3. Android통신(개발자가 응답받음) - CMRespDto
+		return Script.back(e.getErrorMap().toString());
 	}
 }
