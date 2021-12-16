c9e6192d115bfa653ac08671feda321026f4bd7b
gesture02 <gesture02@gmail.com>
2021-12-16 14:42:11 +0900

    구독

---
+++ photogram/src/main/java/com/cos/photogramstart/domain/subscribe/SubscribeRepository.java
@@ -0,0 +1,17 @@
+package com.cos.photogramstart.domain.subscribe;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface SubscribeRepository extends JpaRepository<Subscribe, Integer>{
+	
+	@Modifying //DB에 변경을 주는 쿼리에는 이게 필요하다. (INSERT, DELETE, UPDATE)
+	@Query(value = "INSERT INTO subscribe(fromUserId, toUserId,createDate) VALUES(:fromUserId, :toUserId, now())", nativeQuery = true)
+	void mSubscribe(int fromUserId, int toUserId);// 성공하면 1, 실패하면 -1 (변경된 행의 개수만큼)
+	
+	@Modifying
+	@Query(value = "DELETE FROM subscribe WHERE fromUserId = :fromUserId AND toUserId = :toUserId", nativeQuery = true)
+	void mUnSubscribe(int fromUserId, int toUserId);
+	
+}
